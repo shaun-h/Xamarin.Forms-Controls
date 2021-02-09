95fda1256a1785eb7a6b1e674e80f556f7de1cfc
Shaun Hevey <shaun.hevey@gmail.com>
2021-02-09 22:18:17 +1100

    setting up new shell project

---
+++ CalendarControl/CalendarControl/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using CalendarControl.Views;
+using Xamarin.Forms;
+
+namespace CalendarControl
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ CalendarControl/CalendarControl/Views/CalendarPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CalendarControl.Views
+{
+    public partial class CalendarPage : ContentPage
+    {
+        public CalendarPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
